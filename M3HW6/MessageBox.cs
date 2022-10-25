7dfea6018a0f6803815c47bfe58268ceecd9f551
Max <maksym.illarionov@nure.ua>
2022-10-25 21:02:11 +0300

    Was implemented task for async await with usin g enum

---
+++ M3HW6/MessageBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace M3HW6
+{
+    public class MessageBox
+    {
+        public event Action<State> CloseWindowHandler;
+
+        public async void Open()
+        {
+            Task.Run(() =>
+            {
+                Console.WriteLine("The window is opened");
+
+                Task.Delay(3000).GetAwaiter().GetResult();
+
+                Console.WriteLine("The window is closed");
+
+                Array states = Enum.GetValues(typeof(State));
+
+                var randomState = (State)states.GetValue(new Random().Next(states.Length));
+
+                CloseWindowHandler(randomState);
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ M3HW6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace M3HW6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var messageBox = new MessageBox();
+
+            messageBox.CloseWindowHandler += (State state) =>
+            {
+                if (state == State.Ok)
+                {
+                    Console.WriteLine("Operation was confirmed");
+                }
+
+                if (state == State.Cancel)
+                {
+                    Console.WriteLine("Operation was rejected");
+                }
+            };
+
+            messageBox.Open();
+        }
+    }
+}
